ee7fadf24ea2dd65da4705e429c733e90ef5b63a
David Wachlin <davidwachlin@gmail.com>
2019-12-15 16:22:13 -0500

    handles mongoose casterror: invalid id"

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,17 @@
+/* eslint-disable node/no-unsupported-features/es-syntax */
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
-    stack: err.stack,
-    error: err
+    stack: err.stack
   });
 };
 
@@ -35,6 +43,9 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
