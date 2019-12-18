58e29cb0a4784ca5ac372c2543863419335f7023
David Wachlin <davidwachlin@gmail.com>
2019-12-17 19:16:33 -0500

    adds password confirm validator to schema

--- models/userModel.js
+++ models/userModel.js
@@ -21,7 +21,13 @@ const userSchema = new mongoose.Schema({
   },
   passwordConfirm: {
     type: String,
-    required: [true, 'Please confirm your password']
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on SAVE!
+      validator: function(el) {
+        return el === this.password;
+      }
+    }
   }
 });
 
