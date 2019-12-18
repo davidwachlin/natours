01d30539087854a228ded1fc79afb3101ba6bd53
David Wachlin <davidwachlin@gmail.com>
2019-12-18 16:46:26 -0500

    adds pre save auth function to user model

--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,12 @@ const userSchema = new mongoose.Schema({
   }
 });
 
+userSchema.pre('save', function(next) {
+  if (!this.isModified('password')) return next();
+
+  
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
